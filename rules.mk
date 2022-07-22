6f4a78031417bc15df196667fade7e5671aec8ec
k.bigwheel <k.bigwheel+eng@gmail.com>
2022-07-23 07:30:56 +0900

    インデントを修正

--- rules.mk
+++ rules.mk
@@ -1,7 +1,7 @@
 SRC += \
-	   util_km.c \
-	   features.c \
-	   disable_ime.c \
-	   auto_disable_ime_timer.c \
-     caravelle_backward_func.c \
-	   auto_disable_ime.c
+	util_km.c \
+	features.c \
+	disable_ime.c \
+	auto_disable_ime_timer.c \
+	caravelle_backward_func.c \
+	auto_disable_ime.c
